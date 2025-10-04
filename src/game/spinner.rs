f638ca3b3f481fc1afe1a2bff335e959e2778d30
Bradley Gannon <bradley@bradleygannon.com>
2025-10-04 19:56:36 -0400

    Fix a bug in pin_sector_collision

--- src/game/spinner.rs
+++ src/game/spinner.rs
@@ -22,20 +22,22 @@ impl Default for Spinner {
 impl Spinner {
     pub fn pin_sector_collision(&self, pin: &PinFlying, sector: &Sector) -> bool {
         let inside_sector_radius = pin.vertical_position.abs() < self.radius;
-        let sector_angle_start = normalize_angle(&(self.angular_position + sector.angle_start));
-        let sector_angle_stop = normalize_angle(&(self.angular_position + sector.angle_stop));
-        let sector_is_facing_down = (sector_angle_start.get::<revolution>()
-            ..sector_angle_stop.get::<revolution>())
+        let sector_angle_start_absolute =
+            normalize_angle(&(self.angular_position + sector.angle_start));
+        let sector_angle_stop_absolute =
+            sector_angle_start_absolute + (sector.angle_stop - sector.angle_start);
+        let sector_is_facing_down = (sector_angle_start_absolute.get::<revolution>()
+            ..sector_angle_stop_absolute.get::<revolution>())
             .contains(&0.75);
         inside_sector_radius && sector_is_facing_down
     }
     pub fn pin_pin_collision(&self, flying_pin: &PinFlying, spinner_pin: &PinOnSpinner) -> bool {
         let inside_pin_radius =
             flying_pin.vertical_position.abs() < self.radius + spinner_pin.length;
-        let pin_angle_start =
-            self.angular_position + spinner_pin.angular_position - spinner_pin.width / 2.;
-        let pin_angle_stop =
-            self.angular_position + spinner_pin.angular_position + spinner_pin.width / 2.;
+        let pin_angle_start = normalize_angle(
+            &(self.angular_position + spinner_pin.angular_position - spinner_pin.width / 2.),
+        );
+        let pin_angle_stop = pin_angle_start + spinner_pin.width;
         let spinner_pin_facing_down = (pin_angle_start.get::<revolution>()
             ..pin_angle_stop.get::<revolution>())
             .contains(&0.75);
